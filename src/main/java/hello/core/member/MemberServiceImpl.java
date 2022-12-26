860d35a0a99e6ad99b369e630b8df46ff08b4348
최승은 <liberalfilm@gmail.com>
2022-12-26 16:32:37 +0900

    2022-12-26-Mon-16:32

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -3,7 +3,6 @@ package hello.core.member;
 public class MemberServiceImpl implements MemberService {
 
     private final MemberRepository memberRepository;
-
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
