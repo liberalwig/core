412c4984e9c9ceac8f41c8cead40f7c1099cbcd1
최승은 <liberalfilm@gmail.com>
2022-12-26 17:11:08 +0900

    2022-12-26-Mon-17:10 Converted into SpringBean

--- src/main/java/hello/core/order/MemberApp.java
+++ src/main/java/hello/core/order/MemberApp.java
@@ -1,15 +1,17 @@
 package hello.core.order;
 
-import hello.core.member.Grade;
-import hello.core.member.Member;
-import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
+import hello.core.member.*;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class  MemberApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        // MemberService memberService = new MemberServiceImpl();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+//        applicationContext.getBean("memberRepository", MemberRepository.class);
 
         Member member = new Member(1L, "MemberA", Grade.VIP);
         memberService.join(member);
